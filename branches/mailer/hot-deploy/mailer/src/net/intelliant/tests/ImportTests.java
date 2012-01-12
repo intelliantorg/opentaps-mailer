c0cf0152a31d4511962d0797c9cf59b17d06711f
sourav.biswas <sourav.biswas@12b2f3e6-3aca-11e1-8d71-23de40870312>
2012-01-12 11:30:06 +0000

    Test case of createMarketingCampaign service completed.

--- branches/mailer/hot-deploy/mailer/src/net/intelliant/tests/ImportTests.java
+++ branches/mailer/hot-deploy/mailer/src/net/intelliant/tests/ImportTests.java
@@ -3,5 +3,17 @@ package net.intelliant.tests;
 import org.opentaps.tests.OpentapsTestCase;
 
 public class ImportTests extends OpentapsTestCase {
+	@Override
+	public void setUp() throws Exception {
+		super.setUp();
+	}
 
+	@Override
+	public void tearDown() throws Exception {
+		super.tearDown();
+	}
+	
+	public void testTest(){
+		assertTrue(true);
+	}
 }
--- branches/mailer/hot-deploy/mailer/src/net/intelliant/tests/MarketingCampaignTests.java
+++ branches/mailer/hot-deploy/mailer/src/net/intelliant/tests/MarketingCampaignTests.java
@@ -1,8 +1,18 @@
 package net.intelliant.tests;
 
+import java.util.Map;
+
+import org.ofbiz.base.util.Debug;
+import org.ofbiz.base.util.GeneralException;
+import org.ofbiz.base.util.UtilMisc;
+import org.ofbiz.entity.GenericDelegator;
+import org.ofbiz.entity.GenericEntityException;
+import org.ofbiz.service.GenericServiceException;
+import org.ofbiz.service.LocalDispatcher;
 import org.opentaps.tests.OpentapsTestCase;
 
 public class MarketingCampaignTests extends OpentapsTestCase {
+	private final static String module = MarketingCampaignTests.class.getName();
 
 	@Override
 	public void setUp() throws Exception {
@@ -14,4 +24,60 @@ public class MarketingCampaignTests extends OpentapsTestCase {
 		super.tearDown();
 	}
 
+	public void testCreateMarketingCampaign() throws GeneralException {
+		String campaignName = "Sample test campaign";
+		String fromEmailAddress = "Sample test address";
+		String templateId = "10000";
+		String contractListId = "10000";
+		Double budgetedCost = new Double("12000.00");
+		Double estimatedCost = new Double("11500.50");
+		String currencyUomId = "INR";
+
+		Map<String, Object> inputs = UtilMisc.toMap("campaignName", "Sample test campaign");
+		inputs.put("userLogin", admin);
+		inputs.put("campaignName", campaignName);
+		inputs.put("fromEmailAddress", fromEmailAddress);
+		inputs.put("templateId", templateId);
+		inputs.put("contactListId", contractListId);
+		inputs.put("budgetedCost", budgetedCost);
+		inputs.put("currencyUomId", currencyUomId);
+		inputs.put("estimatedCost", estimatedCost);
+
+		Map<?, ?> results = runAndAssertServiceSuccess("mailer.createMarketingCampaign", inputs);
+		String marketingCampaignId = (String) results.get("marketingCampaignId");
+
+		results = delegator.findByPrimaryKey("MarketingCampaign", UtilMisc.toMap("marketingCampaignId", marketingCampaignId));
+		assertNotNull(results);
+		assertEquals(results.get("campaignName"), campaignName);
+		assertEquals(results.get("budgetedCost"), budgetedCost);
+		assertEquals(results.get("estimatedCost"), estimatedCost);
+		assertEquals(results.get("currencyUomId"), currencyUomId);
+
+		results = delegator.findByPrimaryKey("MailerMarketingCampaign", UtilMisc.toMap("marketingCampaignId", marketingCampaignId));
+		assertNotNull(results);
+		assertEquals(results.get("fromEmailAddress"), fromEmailAddress);
+		assertEquals(results.get("templateId"), templateId);
+		assertEquals(results.get("contactListId"), contractListId);
+
+	}
+
+	public void testCreateMarketingCampaignNoTemplateId() throws GeneralException {
+		String campaignName = "Sample test campaign";
+		String fromEmailAddress = "Sample test address";
+		String contractListId = "10000";
+		Double budgetedCost = new Double("12000.00");
+		Double estimatedCost = new Double("11500.50");
+		String currencyUomId = "INR";
+
+		Map<String, Object> inputs = UtilMisc.toMap("campaignName", "Sample test campaign");
+		inputs.put("userLogin", admin);
+		inputs.put("campaignName", campaignName);
+		inputs.put("fromEmailAddress", fromEmailAddress);
+		inputs.put("contactListId", contractListId);
+		inputs.put("budgetedCost", budgetedCost);
+		inputs.put("currencyUomId", currencyUomId);
+		inputs.put("estimatedCost", estimatedCost);
+
+		runAndAssertServiceFailure("mailer.createMarketingCampaign", inputs);
+	}
 }
