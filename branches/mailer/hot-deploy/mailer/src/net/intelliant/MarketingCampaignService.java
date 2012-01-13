0a6653e983b44b5b4287efe0b74d862e439a25d3
varun.bhansaly <varun.bhansaly@12b2f3e6-3aca-11e1-8d71-23de40870312>
2012-01-13 09:30:22 +0000

    part of r61

--- branches/mailer/hot-deploy/mailer/src/net/intelliant/MarketingCampaignService.java
+++ branches/mailer/hot-deploy/mailer/src/net/intelliant/MarketingCampaignService.java
@@ -1,9 +1,11 @@
 package net.intelliant;
 
 import java.sql.Timestamp;
+import java.util.List;
 import java.util.Locale;
 import java.util.Map;
 
+import org.ofbiz.base.util.Debug;
 import org.ofbiz.base.util.UtilDateTime;
 import org.ofbiz.base.util.UtilMisc;
 import org.ofbiz.base.util.UtilProperties;
@@ -11,6 +13,10 @@ import org.ofbiz.base.util.UtilValidate;
 import org.ofbiz.entity.GenericDelegator;
 import org.ofbiz.entity.GenericEntityException;
 import org.ofbiz.entity.GenericValue;
+import org.ofbiz.entity.condition.EntityConditionList;
+import org.ofbiz.entity.condition.EntityExpr;
+import org.ofbiz.entity.condition.EntityOperator;
+import org.ofbiz.entity.util.EntityUtil;
 import org.ofbiz.service.DispatchContext;
 import org.ofbiz.service.GenericServiceException;
 import org.ofbiz.service.LocalDispatcher;
@@ -33,7 +39,6 @@ public class MarketingCampaignService {
 		Map<String, Object> serviceResults = ServiceUtil.returnSuccess();
 		Locale locale = (Locale) context.get("locale");
 		String contactListId = (String) context.get("contactListId");
-		Timestamp now = UtilDateTime.nowTimestamp();
 		try {
 			GenericValue mergeFormGV = delegator.findByPrimaryKey("MergeForm", UtilMisc.toMap("mergeFormId", context.get("templateId")));
 			if (UtilValidate.isEmpty(mergeFormGV)) {
@@ -55,10 +60,7 @@ public class MarketingCampaignService {
 			GenericValue mailerMarketingCampaign = delegator.makeValue("MailerMarketingCampaign", inputs);
 			mailerMarketingCampaign.create();
 			
-			String campaignListId = delegator.getNextSeqId("MailerMarketingCampaignAndContactList");
-			inputs = UtilMisc.toMap("campaignListId", campaignListId, "marketingCampaignId", marketingCampaignId, "contactListId", contactListId, "fromDate", now);
-			GenericValue mailerMarketingCampaignContactList = delegator.makeValue("MailerMarketingCampaignAndContactList", inputs);
-			mailerMarketingCampaignContactList.create();
+			addContactListToCampaign(delegator, contactListId, marketingCampaignId, null);
 		} catch (GenericEntityException e) {
 			return UtilMessage.createAndLogServiceError(UtilProperties.getMessage(errorResource, "errorCreatingCampaign", locale), module);
 		} catch (GenericServiceException e) {
@@ -66,4 +68,70 @@ public class MarketingCampaignService {
 		}
 		return serviceResults;
 	}
+
+	@SuppressWarnings("unchecked")
+	private static void addContactListToCampaign(GenericDelegator delegator, String contactListId, String marketingCampaignId, Timestamp now) throws GenericEntityException {
+		if (now == null) {
+			now = UtilDateTime.nowTimestamp();
+		}
+		String campaignListId = delegator.getNextSeqId("MailerMarketingCampaignAndContactList");
+		Map<String, Object> inputs = UtilMisc.toMap("campaignListId", campaignListId, "marketingCampaignId", marketingCampaignId, "contactListId", contactListId, "fromDate", now);
+		GenericValue mailerMarketingCampaignContactList = delegator.makeValue("MailerMarketingCampaignAndContactList", inputs);
+		mailerMarketingCampaignContactList.create();
+	}
+	
+	/** Add contact list to campaign. */
+	@SuppressWarnings("unchecked")
+	public static Map<String, Object> addContactListToCampaign(DispatchContext dctx, Map<String, Object> context) {
+		Map<String, Object> serviceResults = ServiceUtil.returnSuccess();
+		Locale locale = (Locale) context.get("locale");
+		String contactListId = (String) context.get("contactListId");
+		String marketingCampaignId = (String) context.get("marketingCampaignId");
+		List andConditions = UtilMisc.toList(new EntityExpr("marketingCampaignId", EntityOperator.EQUALS, marketingCampaignId));
+		andConditions.add(EntityUtil.getFilterByDateExpr());
+		andConditions.add(new EntityExpr("contactListId", EntityOperator.EQUALS, contactListId));
+		EntityConditionList conditions = new EntityConditionList(andConditions, EntityOperator.AND);
+		try {
+			List marketingCampaignContactLists = dctx.getDelegator().findByCondition("MailerMarketingCampaignAndContactList", conditions, null, UtilMisc.toList("fromDate"));
+			if (UtilValidate.isEmpty(marketingCampaignContactLists)) {
+				addContactListToCampaign(dctx.getDelegator(), contactListId, marketingCampaignId, null);
+			} else {
+				if (Debug.infoOn()) {
+					Debug.logInfo("Contact List - " + contactListId + " already associated with campaign - " + marketingCampaignId, module);
+				}
+			}
+		} catch (GenericEntityException e) {
+			return UtilMessage.createAndLogServiceError(UtilProperties.getMessage(errorResource, "errorAddingContactListToCampaign", locale), module);
+		}
+		return serviceResults;
+	}
+	
+	/**
+     * Removes a contact list from a Marketing campaign with its tracking code by expiring those entities
+     * - expire the MarketingCampaignContactList
+     */
+	@SuppressWarnings("unchecked")
+    public static Map<String, Object> removeContactListFromMarketingCampaign(DispatchContext dctx, Map<String, Object> context) {
+        GenericDelegator delegator = dctx.getDelegator();
+        Locale locale = (Locale) context.get("locale");
+        String campaignListId = (String) context.get("campaignListId");
+        Timestamp now = UtilDateTime.nowTimestamp();
+
+        try {
+            // get the MarketingCampaignContactList
+            GenericValue marketingCampaignContactList = delegator.findByPrimaryKey("MailerMarketingCampaignAndContactList", UtilMisc.toMap("campaignListId", campaignListId));
+            if (UtilValidate.isEmpty(marketingCampaignContactList)) {
+                return UtilMessage.createAndLogServiceError("CrmErrorMarketingCampaignContactListNotFound", UtilMisc.toMap("campaignListId", campaignListId), locale, module);
+            }
+            // expire it
+            if (UtilValidate.isEmpty(marketingCampaignContactList.get("thruDate"))) {
+                marketingCampaignContactList.set("thruDate", now);
+                delegator.store(marketingCampaignContactList);
+            }
+
+            return ServiceUtil.returnSuccess();
+        } catch (GenericEntityException e2) {
+            return UtilMessage.createAndLogServiceError(e2, module);
+        }
+    }
 }
