2cda801dc5713ff039e6e47b1dd291089821c112
sourav.biswas <sourav.biswas@12b2f3e6-3aca-11e1-8d71-23de40870312>
2012-01-13 12:56:16 +0000

    Update campaign module with test case created .

--- branches/mailer/hot-deploy/mailer/src/net/intelliant/tests/MarketingCampaignTests.java
+++ branches/mailer/hot-deploy/mailer/src/net/intelliant/tests/MarketingCampaignTests.java
@@ -31,6 +31,27 @@ public class MarketingCampaignTests extends OpentapsTestCase {
 		Double estimatedCost = new Double("11500.50");
 		String currencyUomId = "INR";
 
+		String marketingCampaignId = createMarketingCampaign(campaignName, fromEmailAddress, templateId, contactListId, budgetedCost, estimatedCost, currencyUomId);
+		
+		Map<?, ?> results = delegator.findByPrimaryKey("MarketingCampaign", UtilMisc.toMap("marketingCampaignId", marketingCampaignId));
+		assertNotNull(results);
+		assertEquals(results.get("campaignName"), campaignName);
+		assertEquals(results.get("budgetedCost"), budgetedCost);
+		assertEquals(results.get("estimatedCost"), estimatedCost);
+		assertEquals(results.get("currencyUomId"), currencyUomId);
+		assertEquals(results.get("statusId"), "MKTG_CAMP_PLANNED");
+		/** Should be planned by default. */
+
+		results = delegator.findByPrimaryKey("MailerMarketingCampaign", UtilMisc.toMap("marketingCampaignId", marketingCampaignId));
+		assertNotNull(results);
+		assertEquals(results.get("fromEmailAddress"), fromEmailAddress);
+		assertEquals(results.get("templateId"), templateId);
+
+		List<?> contactListsForMMC = delegator.findByAnd("MailerMarketingCampaignAndContactList", UtilMisc.toMap("marketingCampaignId", marketingCampaignId, "contactListId", contactListId));
+		assertNotEmpty("There must be atleast one active relation between MC and CL", EntityUtil.filterByDate(contactListsForMMC));
+	}
+
+	private String createMarketingCampaign(String campaignName, String fromEmailAddress, String templateId, String contactListId, Double budgetedCost, Double estimatedCost, String currencyUomId) {
 		Map<String, Object> inputs = UtilMisc.toMap("campaignName", campaignName);
 		inputs.put("userLogin", admin);
 		inputs.put("fromEmailAddress", fromEmailAddress);
@@ -43,22 +64,7 @@ public class MarketingCampaignTests extends OpentapsTestCase {
 
 		Map<?, ?> results = runAndAssertServiceSuccess("mailer.createMarketingCampaign", inputs);
 		String marketingCampaignId = (String) results.get("marketingCampaignId");
-
-		results = delegator.findByPrimaryKey("MarketingCampaign", UtilMisc.toMap("marketingCampaignId", marketingCampaignId));
-		assertNotNull(results);
-		assertEquals(results.get("campaignName"), campaignName);
-		assertEquals(results.get("budgetedCost"), budgetedCost);
-		assertEquals(results.get("estimatedCost"), estimatedCost);
-		assertEquals(results.get("currencyUomId"), currencyUomId);
-		assertEquals(results.get("statusId"), "MKTG_CAMP_PLANNED"); /** Should be planned by default. */
-
-		results = delegator.findByPrimaryKey("MailerMarketingCampaign", UtilMisc.toMap("marketingCampaignId", marketingCampaignId));
-		assertNotNull(results);
-		assertEquals(results.get("fromEmailAddress"), fromEmailAddress);
-		assertEquals(results.get("templateId"), templateId);
-		
-		List<?> contactListsForMMC = delegator.findByAnd("MailerMarketingCampaignAndContactList", UtilMisc.toMap("marketingCampaignId", marketingCampaignId, "contactListId", contactListId));
-		assertNotEmpty("There must be atleast one active relation between MC and CL", EntityUtil.filterByDate(contactListsForMMC));
+		return marketingCampaignId;
 	}
 
 	@SuppressWarnings("unchecked")
@@ -81,5 +87,58 @@ public class MarketingCampaignTests extends OpentapsTestCase {
 		inputs.put("estimatedCost", estimatedCost);
 
 		runAndAssertServiceError("mailer.createMarketingCampaign", inputs);
+	}
+
+	public void testUpdateMarketingCampaign() throws GeneralException {
+		Long currTime = System.currentTimeMillis();
+		String campaignName = "Campaign_" + currTime;
+		String fromEmailAddress = "email_" + currTime + "@email.com";
+		String templateId = "10000";
+		String contactListId = "10000";
+		Double budgetedCost = Math.random() * 100000;
+		Double estimatedCost = budgetedCost>1000?budgetedCost-900:budgetedCost-1;
+		String currencyUomId = "INR";
+		
+		// Creating marketing campaign
+		String marketingCampaignId = createMarketingCampaign(campaignName, fromEmailAddress, templateId, contactListId, budgetedCost, estimatedCost, currencyUomId);
+
+		// Assigning updated values. This values will be updated through service
+		currTime = System.currentTimeMillis();
+		campaignName = "Campaign_" + currTime;
+		fromEmailAddress = "email_" + currTime + "@email.com";
+		templateId = "10000";
+		contactListId = "10000";
+		budgetedCost = 12050.0;
+		estimatedCost = 11550.50;
+		currencyUomId = "INR";
+		
+		// Creating map with the updated values.
+		Map<String, Object> inputs = UtilMisc.toMap("marketingCampaignId", marketingCampaignId);
+		inputs.put("campaignName", campaignName);
+		inputs.put("userLogin", admin);
+		inputs.put("fromEmailAddress", fromEmailAddress);
+		inputs.put("templateId", templateId);
+		//inputs.put("contactListId", contactListId);
+		inputs.put("budgetedCost", budgetedCost);
+		inputs.put("currencyUomId", currencyUomId);
+		inputs.put("estimatedCost", estimatedCost);
+		inputs.put("statusId", "MKTG_CAMP_PLANNED");
+		 
+		// Running update service & check for errors.
+		runAndAssertServiceSuccess("mailer.updateMarketingCampaign", inputs);
+		
+		// Checking underlying entity is updated or not - 1
+		Map<?, ?> results = delegator.findByPrimaryKey("MarketingCampaign", UtilMisc.toMap("marketingCampaignId", marketingCampaignId));
+		assertNotNull(results);
+		assertEquals(results.get("campaignName"), campaignName);
+		assertEquals(results.get("budgetedCost"), budgetedCost);
+		assertEquals(results.get("estimatedCost"), estimatedCost);
+		assertEquals(results.get("currencyUomId"), currencyUomId);
+
+		// Checking underlying entity is updated or not - 2
+		results = delegator.findByPrimaryKey("MailerMarketingCampaign", UtilMisc.toMap("marketingCampaignId", marketingCampaignId));
+		assertNotNull(results);
+		assertEquals(results.get("fromEmailAddress"), fromEmailAddress);
+		assertEquals(results.get("templateId"), templateId);
 	}
 }
