497a2d4784bd4247d80d6aad90baf02a3c5e354d
varun.bhansaly <varun.bhansaly@intelliant.net>
2012-01-16 10:38:35 +0000

    excel reader & model reader moved to java methods.

---
+++ src/net/intelliant/imports/UtilImport.java
@@ -0,0 +1,73 @@
+package net.intelliant.imports;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+
+import org.apache.poi.hssf.usermodel.HSSFCell;
+import org.apache.poi.hssf.usermodel.HSSFRow;
+import org.apache.poi.hssf.usermodel.HSSFSheet;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.poifs.filesystem.POIFSFileSystem;
+import org.ofbiz.base.util.UtilMisc;
+import org.ofbiz.base.util.UtilValidate;
+import org.ofbiz.entity.GenericEntityException;
+import org.ofbiz.entity.model.ModelEntity;
+import org.ofbiz.entity.model.ModelField;
+import org.ofbiz.entity.model.ModelReader;
+
+public class UtilImport {	
+	private UtilImport() {}
+	
+	public static List<Map<String, Object>> getEntityColumns(String entityName, List<String> entityColumnsToIgnore) throws GenericEntityException {
+		if (UtilValidate.isEmpty(entityName)) {
+			return null;
+		}
+		if (UtilValidate.isEmpty(entityColumnsToIgnore)) {
+			entityColumnsToIgnore = new ArrayList<String>();
+		}
+		ModelReader reader = ModelReader.getModelReader("default");
+		ModelEntity modelEntity = reader.getModelEntity(entityName);
+		Iterator<ModelField> fieldIterator = modelEntity.getFieldsIterator();
+		List<Map<String, Object>> lhsColumns = new ArrayList<Map<String, Object>>();
+		while (fieldIterator.hasNext()) {
+		    ModelField field = fieldIterator.next();
+		    String fieldDesc = field.getDescription();
+		    if (UtilValidate.isEmpty(fieldDesc)) {
+		    	fieldDesc = field.getName();
+		    }
+		    if (!entityColumnsToIgnore.contains(field.getName()) && !field.getIsPk()) {
+		    	lhsColumns.add(UtilMisc.toMap("entityColName", field.getName(), "entityColDesc", fieldDesc));
+		    }
+		}
+		return lhsColumns;		
+	}
+	
+	public static List<Integer> readExcelIndices(String excelFilePath, int sheetIndex) throws FileNotFoundException, IOException {
+		List<Integer> columnIndices = new ArrayList<Integer>();
+		File file = new File(excelFilePath);
+		if (file != null && file.canRead()) {
+			POIFSFileSystem fs = new POIFSFileSystem(new FileInputStream(file));
+			HSSFWorkbook wb = new HSSFWorkbook(fs);
+//			HSSFSheet sheet = wb.getSheet(wb.getActiveSheetIndex());
+			HSSFSheet sheet = wb.getSheetAt(sheetIndex);
+			if (sheet != null) {
+				HSSFRow firstRow = sheet.getRow(sheet.getFirstRowNum());
+				if (firstRow != null) {
+					firstRow.getPhysicalNumberOfCells();
+					Iterator<?> cells = firstRow.cellIterator();
+					while (cells.hasNext()) {
+						HSSFCell cell = (HSSFCell) cells.next();
+						columnIndices.add(Integer.valueOf(cell.getCellNum())); // this gives the cells with data.
+					}
+				}
+			}
+		}
+		return columnIndices;
+	}
+}
