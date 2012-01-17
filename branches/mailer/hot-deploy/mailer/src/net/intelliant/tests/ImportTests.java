a6657990aa3dbdc02e2684cb936b809ecf4b532d
varun.bhansaly <varun.bhansaly@12b2f3e6-3aca-11e1-8d71-23de40870312>
2012-01-17 06:21:15 +0000

    test case to validate reading of excel column indices.

--- branches/mailer/hot-deploy/mailer/src/net/intelliant/tests/ImportTests.java
+++ branches/mailer/hot-deploy/mailer/src/net/intelliant/tests/ImportTests.java
@@ -1,5 +1,14 @@
 package net.intelliant.tests;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import net.intelliant.imports.UtilImport;
+
+import org.ofbiz.base.util.GeneralException;
 import org.opentaps.tests.OpentapsTestCase;
 
 public class ImportTests extends OpentapsTestCase {
@@ -13,7 +22,30 @@ public class ImportTests extends OpentapsTestCase {
 		super.tearDown();
 	}
 	
-	public void testTest(){
-		assertTrue(true);
+	public void testReadExcelIndices() throws GeneralException {
+		String ofbizHome = System.getProperty("ofbiz.home");
+		if (!ofbizHome.endsWith(File.separator)) {
+			ofbizHome += File.separator;
+		}
+		String tokenizedPath = "hot-deploy/mailer/src/net/intelliant/tests/xls/import_sample.xls";
+		String excelFilePath = ofbizHome + tokenizedPath.replaceAll("/", File.separator);
+		try {
+			List<Integer> expectedIndices = new ArrayList<Integer>();
+			expectedIndices.add(0);
+			expectedIndices.add(1);
+			List<Integer> actualIndices = UtilImport.readExcelIndices(excelFilePath, 0);
+			assertEquals(expectedIndices, actualIndices);
+			
+			expectedIndices = new ArrayList<Integer>();
+			expectedIndices.add(1);
+			expectedIndices.add(2);
+			expectedIndices.add(3);
+			actualIndices = UtilImport.readExcelIndices(excelFilePath, 1);
+			assertEquals(expectedIndices, actualIndices);			
+		} catch (FileNotFoundException e) {
+			fail(e.getMessage());
+		} catch (IOException e) {
+			fail(e.getMessage());
+		}
 	}
 }
