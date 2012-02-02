96b6caff01ca2c2a29b3bcde8c8faa04ab7dc6bf
varun.bhansaly <varun.bhansaly@12b2f3e6-3aca-11e1-8d71-23de40870312>
2012-02-02 09:33:51 +0000

    unnecessary snippet.

--- branches/mailer/hot-deploy/mailer/webapp/mailer/WEB-INF/actions/getEntityColumnList.groovy
+++ branches/mailer/hot-deploy/mailer/webapp/mailer/WEB-INF/actions/getEntityColumnList.groovy
@@ -3,5 +3,4 @@ import org.ofbiz.base.util.UtilMisc
 
 entityColumnsToIgnore = UtilMisc.toList("lastUpdatedStamp", "lastUpdatedTxStamp", "createdStamp", "createdTxStamp", "importedOnDateTime", "importedByUserLogin");
 entityColumns = UtilImport.getEntityColumns(context.nameOfEntity, entityColumnsToIgnore);
-//System.out.println("Entity Columns1 : "+entityColumns);
 context.put("entityColumns", entityColumns);
