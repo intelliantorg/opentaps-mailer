f33625bb2e41f55d52e2e3bc51c6360f64f70a32
varun.bhansaly <varun.bhansaly@12b2f3e6-3aca-11e1-8d71-23de40870312>
2012-02-02 09:39:20 +0000

    more fixes.

--- branches/mailer/hot-deploy/mailer/webapp/mailer/WEB-INF/actions/viewImportColumnMappingListing.groovy
+++ branches/mailer/hot-deploy/mailer/webapp/mailer/WEB-INF/actions/viewImportColumnMappingListing.groovy
@@ -10,26 +10,26 @@ import org.ofbiz.entity.GenericValue;
 importMapperId = parameters.importMapperId;
 
 importMapperGV = delegator.findByPrimaryKey("MailerImportMapper", UtilMisc.toMap("importMapperId", importMapperId));
-mailerImportColumnMapperList = importMapperGV.getRelatedByAnd("MailerImportColumnMapper", UtilMisc.toMap("importMapperId", importMapperId));
+mailerImportColumnMapperList = importMapperGV.getRelated("MailerImportColumnMapper");
 
 selectedColumnIndexMap = FastMap.newInstance();
-for(GenericValue mailerImportColumnMapper : mailerImportColumnMapperList){
-	selectedColumnIndexMap.putAt(mailerImportColumnMapper.entityColName,mailerImportColumnMapper.importFileColIdx);
+for (GenericValue mailerImportColumnMapper : mailerImportColumnMapperList) {
+	selectedColumnIndexMap.putAt(mailerImportColumnMapper.entityColName, mailerImportColumnMapper.importFileColIdx);
 }
 
 dataResourceGV = importMapperGV.getRelatedOne("Content").getRelatedOne("DataResource");
 filePath = null;
-if(UtilValidate.isNotEmpty(dataResourceGV)){
+if (UtilValidate.isNotEmpty(dataResourceGV)) {
 	filePath = dataResourceGV.get("objectInfo");
 }
 
-if(UtilValidate.isNotEmpty(filePath)){
+if (UtilValidate.isNotEmpty(filePath)) {
 	entityColumnsToIgnore = UtilMisc.toList("lastUpdatedStamp", "lastUpdatedTxStamp", "createdStamp", "createdTxStamp", "importedOnDateTime", "importedByUserLogin");
 	lhsColumns = UtilImport.getEntityColumns(context.nameOfEntity, entityColumnsToIgnore);
 	context.put("lhsColumns", lhsColumns);
 	
 	selectedIndexes = FastList.newInstance();
-	for(String colName : lhsColumns){
+	for (String colName : lhsColumns) {
 		index = selectedColumnIndexMap.get(colName.entityColName);
 		selectedIndexes.add((index != null) ? index : -1);
 	}
