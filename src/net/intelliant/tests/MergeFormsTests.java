fc33ce33b0cb9e4398b3a7c8c5317ebbc2821174
varun.bhansaly <varun.bhansaly@intelliant.net>
2012-02-09 10:42:06 +0000

    Test case fixed.

--- src/net/intelliant/tests/MergeFormsTests.java
+++ src/net/intelliant/tests/MergeFormsTests.java
@@ -23,16 +23,14 @@ public class MergeFormsTests extends MailerTests {
 		Long currTime = System.currentTimeMillis();
 
 		String mergeFormId;
-		String partyId = "10083";
 		String mergeFormName = "Test form name - " + currTime;
 		String subject = "Test subject - " + currTime;
-		String scheduleAt = "" + (int)Math.random()*10000;
+		String scheduleAt = "" + (int)Math.random() * 10000;
 		String description = "Test description - " + currTime;
 		String mergeFormText = "Test merge form text - " + currTime;
 		String showInSelect = (Math.random() > .5) ? "Y" : "N";
 
 		Map<String, Object> inputs = UtilMisc.toMap("userLogin", admin);
-		inputs.put("partyId", partyId);
 		inputs.put("mergeFormName", mergeFormName);
 		inputs.put("subject", subject);
 		inputs.put("scheduleAt", scheduleAt);
@@ -51,19 +49,5 @@ public class MergeFormsTests extends MailerTests {
 		assertEquals(results.get("description"), description);
 		assertEquals(results.get("mergeFormText"), mergeFormText);
 		assertEquals(results.get("showInSelect"), showInSelect);
-
-		partyId = "10083";
-		subject = "Test subject - " + currTime;
-		description = "Test description - " + currTime;
-		showInSelect = (Math.random() > .5) ? "Y" : "N";
-
-		inputs.clear();
-		inputs = UtilMisc.toMap("userLogin", admin);
-		inputs.put("partyId", partyId);
-		inputs.put("subject", subject);
-		inputs.put("description", description);
-		inputs.put("showInSelect", showInSelect);
-
-		runAndAssertServiceFailure("mailer.createMergeForm", inputs);
 	}
 }
