ebaa95ead9e48abaaafe231c21a0ee5cc087a014
varun.bhansaly <varun.bhansaly@intelliant.net>
2012-02-14 05:58:02 +0000

    New Feature : show campaign line count for this contact list + marketing campaign combination.

--- src/net/intelliant/util/UtilCommon.java
+++ src/net/intelliant/util/UtilCommon.java
@@ -1,12 +1,16 @@
 package net.intelliant.util;
 
 import java.io.File;
+import java.util.Map;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 import org.ofbiz.base.util.Debug;
+import org.ofbiz.base.util.UtilMisc;
 import org.ofbiz.base.util.UtilValidate;
+import org.ofbiz.entity.GenericDelegator;
+import org.ofbiz.entity.GenericEntityException;
 
 public final class UtilCommon {
 	private static final String module = UtilCommon.class.getName();
@@ -35,5 +39,33 @@ public final class UtilCommon {
 	 */
 	public static String getUploadPath() {
 		return System.getProperty("user.dir") + File.separatorChar + "runtime" + File.separatorChar + "data" + File.separatorChar;
-	}  
+	}
+	
+	@SuppressWarnings("unchecked")
+	private static long countCampaignLines(GenericDelegator delegator, String statusId, String contactListId, String marketingCampaignId) throws GenericEntityException {
+		Map<String, Object> conditions = UtilMisc.toMap("statusId", statusId);
+		if (UtilValidate.isNotEmpty(contactListId)) {
+			conditions.put("contactListId", contactListId);
+		}
+		if (UtilValidate.isNotEmpty(marketingCampaignId)) {
+			conditions.put("marketingCampaignId", marketingCampaignId);
+		}
+		return delegator.findCountByAnd("MailerCampaignStatus", conditions);
+	}
+	
+	public static long countScheduledCampaignLines(GenericDelegator delegator, String contactListId, String marketingCampaignId) throws GenericEntityException {
+		return countCampaignLines(delegator, "MAILER_SCHEDULED", contactListId, marketingCampaignId);
+	}
+	
+	public static long countCancelledCampaignLines(GenericDelegator delegator, String contactListId, String marketingCampaignId) throws GenericEntityException {
+		return countCampaignLines(delegator, "MAILER_CANCELLED", contactListId, marketingCampaignId);
+	}
+	
+	public static long countExecutedCampaignLines(GenericDelegator delegator, String contactListId, String marketingCampaignId) throws GenericEntityException {
+		return countCampaignLines(delegator, "MAILER_EXECUTED", contactListId, marketingCampaignId);
+	}
+	
+	public static long countOnHoldCampaignLines(GenericDelegator delegator, String contactListId, String marketingCampaignId) throws GenericEntityException {
+		return countCampaignLines(delegator, "MAILER_HOLD", contactListId, marketingCampaignId);
+	}
 }
