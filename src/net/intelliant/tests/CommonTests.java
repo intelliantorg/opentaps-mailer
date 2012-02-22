b605051e3b9350273d07e2610503be5784bf9dbf
varun.bhansaly <varun.bhansaly@intelliant.net>
2012-02-22 13:28:38 +0000

    test case to validate functioning of method introduced in r409

--- src/net/intelliant/tests/CommonTests.java
+++ src/net/intelliant/tests/CommonTests.java
@@ -2,7 +2,11 @@ package net.intelliant.tests;
 
 import net.intelliant.util.UtilCommon;
 
+import org.ofbiz.base.util.UtilDateTime;
+import org.ofbiz.base.util.UtilMisc;
 import org.ofbiz.entity.GenericEntityException;
+import org.ofbiz.entity.GenericValue;
+import org.ofbiz.entity.util.EntityUtil;
 
 public class CommonTests extends MailerTests {
 	
@@ -20,5 +24,16 @@ public class CommonTests extends MailerTests {
 		assertEquals(false, UtilCommon.isValidEmailAddress("."));
 		assertEquals(false, UtilCommon.isValidEmailAddress(".email@domain"));
 		assertEquals(false, UtilCommon.isValidEmailAddress(""));
+	}
+	
+	public void testContactListRecipientCount() throws GenericEntityException {
+		String contactListId = createContactListWithTwoRecipients();
+		assertEquals(2, UtilCommon.countContactListRecipients(delegator, contactListId));
+		
+		GenericValue anyRecipient = EntityUtil.getFirst(delegator.findByAnd("MailerRecipientContactList", UtilMisc.toMap("contactListId", contactListId)));
+		anyRecipient.set("validThruDate", UtilDateTime.nowTimestamp());
+		anyRecipient.store();
+		
+		assertEquals(1, UtilCommon.countContactListRecipients(delegator, contactListId));
 	}
 }
