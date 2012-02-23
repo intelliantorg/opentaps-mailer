c7264a101fe3bffbc8927bf5cd8ab2ca467de879
varun.bhansaly <varun.bhansaly@12b2f3e6-3aca-11e1-8d71-23de40870312>
2012-02-23 10:15:02 +0000

    test case to validate removal of recipient from contact list.

--- branches/mailer/hot-deploy/mailer/src/net/intelliant/tests/ContactListTests.java
+++ branches/mailer/hot-deploy/mailer/src/net/intelliant/tests/ContactListTests.java
@@ -3,9 +3,15 @@ package net.intelliant.tests;
 import java.util.List;
 import java.util.Map;
 
+import net.intelliant.util.UtilCommon;
+
 import org.ofbiz.base.util.GeneralException;
 import org.ofbiz.base.util.UtilMisc;
 import org.ofbiz.entity.GenericValue;
+import org.ofbiz.entity.condition.EntityCondition;
+import org.ofbiz.entity.condition.EntityConditionList;
+import org.ofbiz.entity.condition.EntityExpr;
+import org.ofbiz.entity.condition.EntityOperator;
 import org.ofbiz.entity.util.EntityUtil;
 
 public class ContactListTests extends MailerTests {
@@ -20,6 +26,7 @@ public class ContactListTests extends MailerTests {
 		super.tearDown();
 	}
 
+	@SuppressWarnings("unchecked")
 	public void testCreateContactListWithMailerCampaignInInput() throws GeneralException {
 		String campaignName = "Campaign_" + System.currentTimeMillis();
 		String templateId = createMergeTemplate(null);
@@ -49,6 +56,91 @@ public class ContactListTests extends MailerTests {
 		assertEquals(1, relations.size());
 		GenericValue relation = EntityUtil.getFirst(relations);
 		assertEquals(marketingCampaignId, relation.getString("marketingCampaignId"));
+	}
+
+	@SuppressWarnings("unchecked")
+	public void testRemoveRecipientFromContactListWithNoMailerCampaign() throws GeneralException {
+		String contactListId = createContactListWithTwoRecipients();
 		
+		List<GenericValue> relations = delegator.findByAnd("MailerRecipientContactList", UtilMisc.toMap("contactListId", contactListId));
+		assertEquals(2, relations.size());
+		GenericValue relation = EntityUtil.getFirst(relations);
+		String recipientListId = relation.getString("recipientListId");
+		
+		runAndAssertServiceSuccess("mailer.removeRecipientFromContactList", UtilMisc.toMap("userLogin", admin, "recipientListId", recipientListId, "contactListId", contactListId));
+		long activeRecipients = UtilCommon.countContactListRecipients(delegator, contactListId);
+		assertEquals(1, activeRecipients);
+	}
+	
+	@SuppressWarnings("unchecked")
+	public void testRemoveRecipientFromContactListWithMailerCampaign() throws GeneralException {
+		String contactListId = createContactListWithTwoRecipients();
+		
+		String campaignName = "Campaign_" + System.currentTimeMillis();
+		String templateId = createMergeTemplate(null);
+		Double budgetedCost = new Double("12000.00");
+		Double estimatedCost = new Double("11500.50");
+		String currencyUomId = "INR";
+		String description = "Some description for this campaign";
+		
+		Map<String, Object> inputs = UtilMisc.toMap("campaignName", campaignName);
+		inputs.put("userLogin", admin);
+		inputs.put("templateId", templateId);
+		inputs.put("contactListId", contactListId);
+		inputs.put("budgetedCost", budgetedCost);
+		inputs.put("currencyUomId", currencyUomId);
+		inputs.put("estimatedCost", estimatedCost);
+		inputs.put("description", description);
+		inputs.put("statusId", "MKTG_CAMP_PLANNED");
+		String marketingCampaignId = createMarketingCampaign(inputs);
+		
+		/** execute all campaigns */
+		List<GenericValue> campaigns = delegator.findByAnd("MailerCampaignStatus", UtilMisc.toMap("marketingCampaignId", marketingCampaignId));
+		for (GenericValue scheduledCampaign : campaigns) {
+			scheduledCampaign.put("statusId", "MAILER_EXECUTED");
+			scheduledCampaign.store();
+		}
+		
+		/** remove one of the recipients */
+		List<GenericValue> relations = delegator.findByAnd("MailerRecipientContactList", UtilMisc.toMap("contactListId", contactListId));
+		assertEquals(2, relations.size());
+		GenericValue relation = EntityUtil.getFirst(relations);
+		String recipientListId = relation.getString("recipientListId");
+		
+		runAndAssertServiceSuccess("mailer.removeRecipientFromContactList", UtilMisc.toMap("userLogin", admin, "recipientListId", recipientListId, "contactListId", contactListId));
+		long activeRecipients = UtilCommon.countContactListRecipients(delegator, contactListId);
+		assertEquals(1, activeRecipients);
+		
+		long cancelledCount = UtilCommon.countCancelledCampaignLines(delegator, contactListId, marketingCampaignId);
+		assertEquals(0, cancelledCount);
+		
+		/** create another campaign. */
+		String marketingCampaignId2 = createMarketingCampaign(inputs);
+		long onHoldCount = UtilCommon.countOnHoldCampaignLines(delegator, contactListId, marketingCampaignId2);
+		long totalCount = UtilCommon.countAllCampaignLines(delegator, contactListId, marketingCampaignId2);
+		assertEquals(1, onHoldCount);
+		assertEquals(1, totalCount);
+		
+		EntityCondition conditions = new EntityConditionList( 
+    		UtilMisc.toList(
+				new EntityExpr("contactListId", EntityOperator.EQUALS, contactListId),
+                EntityUtil.getFilterByDateExpr("validFromDate", "validThruDate")
+    		), EntityOperator.AND
+    	);
+		/** find active recipients. */
+		relations = delegator.findByCondition("MailerRecipientContactList", conditions, null, null);
+		assertEquals(1, relations.size());
+		relation = EntityUtil.getFirst(relations);
+		recipientListId = relation.getString("recipientListId");
+		
+		runAndAssertServiceSuccess("mailer.removeRecipientFromContactList", UtilMisc.toMap("userLogin", admin, "recipientListId", recipientListId));
+		activeRecipients = UtilCommon.countContactListRecipients(delegator, contactListId);
+		assertEquals(0, activeRecipients);
+		
+		cancelledCount = UtilCommon.countCancelledCampaignLines(delegator, contactListId, marketingCampaignId2);
+		assertEquals(1, cancelledCount);
+		
+		totalCount = UtilCommon.countAllCampaignLines(delegator, contactListId, marketingCampaignId2);
+		assertEquals(1, totalCount);
 	}
 }
