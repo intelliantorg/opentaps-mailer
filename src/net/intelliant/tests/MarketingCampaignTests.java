5d4453987b57cbb9c527de53b57cdaa0813d69b6
varun.bhansaly <varun.bhansaly@intelliant.net>
2012-02-25 10:56:05 +0000

    test case to validate thru date cannot be less than from date for update campaign.

--- src/net/intelliant/tests/MarketingCampaignTests.java
+++ src/net/intelliant/tests/MarketingCampaignTests.java
@@ -235,6 +235,39 @@ public class MarketingCampaignTests extends MailerTests {
 	}
 	
 	@SuppressWarnings("unchecked")
+	public void testUpdateMarketingCampaignFailWithFromDateGreaterThanThruDate() throws GeneralException {
+		Timestamp fromDate = UtilDateTime.addDaysToTimestamp(UtilDateTime.nowTimestamp(), 1);
+		Timestamp thruDate = UtilDateTime.addDaysToTimestamp(fromDate, 1);
+		String campaignName = "Campaign_" + System.currentTimeMillis();
+		String templateId = createMergeTemplate(null);
+		String contactListId = createContactList(null);
+		Double budgetedCost = new Double("12000.00");
+		Double estimatedCost = new Double("11500.50");
+		String currencyUomId = "INR";
+		String description = "Some description for this campaign";
+		
+		Map<String, Object> inputs = UtilMisc.toMap("campaignName", campaignName);
+		inputs.put("userLogin", admin);
+		inputs.put("templateId", templateId);
+		inputs.put("contactListId", contactListId);
+		inputs.put("budgetedCost", budgetedCost);
+		inputs.put("currencyUomId", currencyUomId);
+		inputs.put("estimatedCost", estimatedCost);
+		inputs.put("description", description);
+		inputs.put("statusId", "MKTG_CAMP_PLANNED");
+		inputs.put("fromDate", fromDate);
+		inputs.put("thruDate", thruDate);
+		String marketingCampaignId = createMarketingCampaign(inputs);
+		
+		inputs.clear();
+		inputs.put("userLogin", admin);
+		inputs.put("marketingCampaignId", marketingCampaignId);
+		inputs.put("fromDate", fromDate);
+		inputs.put("thruDate", UtilDateTime.addDaysToTimestamp(fromDate, -10));
+		runAndAssertServiceError("mailer.updateMarketingCampaign", inputs);
+	}
+	
+	@SuppressWarnings("unchecked")
 	public void testCancelMarketingCampaignWithNoContactList() throws GeneralException {
 		Timestamp fromDate = UtilDateTime.addDaysToTimestamp(UtilDateTime.nowTimestamp(), 1);
 		Timestamp thruDate = UtilDateTime.addDaysToTimestamp(fromDate, 1);
