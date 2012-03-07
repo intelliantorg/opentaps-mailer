bf8ed340c69e6b02172e560c302650c6319fb32b
varun.bhansaly <varun.bhansaly@12b2f3e6-3aca-11e1-8d71-23de40870312>
2012-03-07 14:20:28 +0000

    semi colons missing, not sure it was working here.

--- branches/mailer/hot-deploy/mailer/webapp/mailer/WEB-INF/actions/getEntityColumnList.groovy
+++ branches/mailer/hot-deploy/mailer/webapp/mailer/WEB-INF/actions/getEntityColumnList.groovy
@@ -1,5 +1,5 @@
-import net.intelliant.imports.UtilImport
-import org.ofbiz.base.util.UtilMisc
+import net.intelliant.imports.UtilImport;
+import org.ofbiz.base.util.UtilMisc;
 
 entityColumnsToIgnore = UtilMisc.toList("lastUpdatedStamp", "lastUpdatedTxStamp", "createdStamp", "createdTxStamp", "importedOnDateTime", "importedByUserLogin");
 entityColumns = UtilImport.getEntityColumns(context.nameOfEntity, entityColumnsToIgnore);
